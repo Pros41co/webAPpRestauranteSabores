5a9c2711bc4030dae267b67a6bac28ac566d1afb
Alex <xan02der@gmail.com>
2025-09-18 18:45:53 -0500

    Update Script.sql
    Se inserta la query para añadir al administrador con clave hasheada.

--- Script.sql
+++ Script.sql
@@ -22,6 +22,8 @@ create table users(
 	foreign key (id_rol) references roles(id)
 )
 
+insert into users(name, lastname, username, email, id_rol, password, identification) values ('admin', 'admin', 'admin', 'nocorreo@correo.com', 2, '$2a$12$lgWRYDr5nILZxRMphvixV.9nfl7xqoTjd79DeRAkayCvt/aO/.GJe', '1');
+
 
 select rol_name, COUNT(*) as count from users left join roles on users.id_rol = roles.id group by id_rol;
 
